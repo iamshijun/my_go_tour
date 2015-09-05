432a37f2ea61605f715f25b97a971bda61414c7d
iamshijun <aimysaber@gmail.com>
2015-09-05 23:37:54 +0900

    add final tour demo

--- main/stringer.go
+++ main/stringer.go
@@ -9,7 +9,7 @@ type Person struct {
 
 func (p Person) String() string {
 	//Sprint donot print out but return the string output
-	return fmt.Sprintf("%v (%v years)", p.Name, p.Age)
+	return fmt.Sprintf("%q (%v years)", p.Name, p.Age)
 }
 
 //Stringer 是一个可以用字符串描述自己的类型。`fmt`包 （还有许多其他包）使用这个来进行输出。
